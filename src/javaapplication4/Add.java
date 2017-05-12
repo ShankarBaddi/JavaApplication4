8db44c1d45c812e06a19bf02bb084013eac4b9e8
shankar <shankar@shankar-PC>
2017-05-12 14:16:29 +0530

    newly added add function

--- src/javaapplication4/Add.java
+++ src/javaapplication4/Add.java
@@ -10,5 +10,11 @@ package javaapplication4;
  * @author shankar
  */
 public class Add {
+    public static void main(String args[]){
+        int d=3;
+        int f=5;
+        int e=d+f;
+        System.out.println();
+    }
     
 }
