6369885cd114cbe4d0b8e73002bcccd7c1e8b0ba
shankar <shankar@shankar-PC>
2017-05-12 16:46:38 +0530

    nan method has added

--- src/javaapplication4/Add.java
+++ src/javaapplication4/Add.java
@@ -10,11 +10,17 @@ package javaapplication4;
  * @author shankar
  */
 public class Add {
+    
+    public void nan(){
+        System.out.println("how r u?");
+    }
     public static void main(String args[]){
         int d=3;
         int f=5;
         int e=d+f;
         System.out.println(e);
+        Add  add=new Add();
+        add.nan();
     }
     
 }
