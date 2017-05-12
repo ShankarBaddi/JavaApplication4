1dddf40c29d53ae06cc0eaa8ba73eeee78ebc50d
shankar <shankar@shankar-PC>
2017-05-12 13:00:41 +0530

    new commit

---
+++ src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author shankar
+ */
+public class JavaApplication4 {
+int a;
+int b;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic herde
+        System.out.println("shankar");
+        System.out.println("nn");
+        System.out.println();
+        
+    }
+    
+}
